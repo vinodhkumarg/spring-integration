179c4efea30cdfb29e8b60fb6234477cec047fb9
vkum102 <icuE@1234>
2018-12-18 19:03:20 +0530

    spring-integration-direct-channel

---
+++ spring-integration-direct-channel/src/main/java/com/test/spring/gateway/PrintGateway.java
@@ -0,0 +1,11 @@
+package com.test.spring.gateway;
+
+import java.util.concurrent.Future;
+
+import org.springframework.messaging.Message;
+
+public interface PrintGateway {
+
+	public void print1(Message<?> String);
+
+}
